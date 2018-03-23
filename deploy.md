33f74e1d561a1de8c4bdf94c3514d4a03ab12ceb
mroloux <matti@seats.io>
2018-03-23 14:18:47 +0100

    Added automated deployment

--- deploy.md
+++
@@ -1,6 +0,0 @@
-How to deploy:
-
-1) cd SeatsioDotNet
-1) increase version number in SeatsioDotNet.csproj
-2) dotnet pack
-3) dotnet nuget push bin/Debug/SeatsioDotNet.<version>.0.0.nupkg -s nuget.org -k <nuget API key>
---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+if [ "$#" -ne 1 ]; then
+  echo "Error: please pass in your nuget.org API key"
+  exit 1
+fi
+
+dotnet pack
+fileName=(`ls -t SeatsioDotNet/bin/Debug/*.nupkg | head -1`)
+dotnet nuget push $fileName -s nuget.org -k $1

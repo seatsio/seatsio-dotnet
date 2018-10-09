1f01f185f21128a94153fdb10e9b891b7f069bef
Ben Verbeken <ben@seats.io>
2018-10-09 10:02:14 +0200

    build fix - validation errors resulted in test failures

--- SeatsioDotNet.Test/Accounts/RetrieveMyAccountTest.cs
+++ SeatsioDotNet.Test/Accounts/RetrieveMyAccountTest.cs
@@ -15,9 +15,9 @@ namespace SeatsioDotNet.Test.Accounts
             Assert.NotNull(account.PublicKey);
             Assert.NotNull(account.Email);
             Assert.True(account.Settings.DraftChartDrawingsEnabled);
-            Assert.Equal(account.Settings.ChartValidation.ValidateDuplicateLabels, ChartValidationLevel.WARNING);
-            Assert.Equal(account.Settings.ChartValidation.ValidateObjectsWithoutCategories, ChartValidationLevel.WARNING);
-            Assert.Equal(account.Settings.ChartValidation.ValidateUnlabeledObjects, ChartValidationLevel.WARNING);
+            Assert.Equal(ChartValidationLevel.ERROR, account.Settings.ChartValidation.ValidateDuplicateLabels);
+            Assert.Equal(ChartValidationLevel.ERROR, account.Settings.ChartValidation.ValidateObjectsWithoutCategories);
+            Assert.Equal(ChartValidationLevel.ERROR, account.Settings.ChartValidation.ValidateUnlabeledObjects);
         }
     }
 }

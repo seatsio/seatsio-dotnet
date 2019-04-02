c98848f1f0be3b7e91464c49ba420dcb3e84057e
mroloux <matti@seats.io>
2019-04-02 10:49:36 +0200

    Exposed isSubaccount flag

--- SeatsioDotNet/Accounts/Account.cs
+++ SeatsioDotNet/Accounts/Account.cs
@@ -6,6 +6,7 @@
         public string DesignerKey { get; set; }
         public string PublicKey { get; set; }
         public string Email { get; set; }
+        public bool IsSubaccount { get; set; }
         public AccountSettings Settings { get; set; }
     }
 }

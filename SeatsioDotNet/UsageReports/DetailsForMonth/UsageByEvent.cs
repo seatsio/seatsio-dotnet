ae1dbbeaa0709354fc807ad5ba12547c9b2601a6
Matti Roloux <matti@seats.io>
2022-06-13 13:13:54 +0200

    Deleted legacy fields from usage reports

--- SeatsioDotNet/UsageReports/DetailsForMonth/UsageByEvent.cs
+++ SeatsioDotNet/UsageReports/DetailsForMonth/UsageByEvent.cs
@@ -7,9 +7,6 @@ namespace SeatsioDotNet.UsageReports
         public UsageEvent Event { get; set; }
         public int NumUsedObjects { get; set; }
         public int NumFirstBookings { get; set; }
-        public int NumFirstBookingsOrSelections { get; set; }
-        public int NumGASelectionsWithoutBooking { get; set; }
-        public int NumNonGASelectionsWithoutBooking { get; set; }
         public int NumObjectSelections { get; set; }
     }
 }

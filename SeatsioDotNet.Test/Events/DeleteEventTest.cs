0dd970260c72116af655b3dcf0be99e540d40fce
Matti Roloux <matti@seats.io>
2023-01-19 17:13:25 +0100

    Fixed test

--- SeatsioDotNet.Test/Events/DeleteEventTest.cs
+++ SeatsioDotNet.Test/Events/DeleteEventTest.cs
@@ -15,7 +15,7 @@ namespace SeatsioDotNet.Test.Events
 
             Exception ex = Assert.Throws<SeatsioException>(() => Client.Events.Retrieve(evnt.Key));
 
-            Assert.Equal("Event not found: 1d7ea65c-fa60-4b68-a0de-3b71affd2e7e.", ex.Message);
+            Assert.Equal("Event not found: " + evnt.Key + ".", ex.Message);
         }
     }
 }

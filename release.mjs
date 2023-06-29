19aad1e8f7d9adba0fbe7becde8b193d6b9e0fa1
Matti Roloux <matti@seats.io>
2023-06-29 11:01:00 +0200

    Don't release when there are no changes

--- release.mjs
+++ release.mjs
@@ -1,32 +1,16 @@
 #!/usr/bin/env zx
 
-/*
-* Script to release the seats.io java lib.
-*   - changes the version number in README.md
-*   - changes the version number in build.gradle
-*   - creates the release in Gihub (using gh cli)
-*
-*
-* Prerequisites:
-*   - zx installed (https://github.com/google/zx)
-*   - gh cli installed (https://cli.github.com/)
-*   - semver cli installed (https://github.com/fsaintjacques/semver-tool)
-*
-* Usage:
-*   zx ./release.mjs -v major/minor -n "release notes"
-* */
-
-// don't output the commands themselves
 $.verbose = false
 
 const versionToBump = getVersionToBump()
-const latestVersion = await fetchLatestReleasedVersionNumber()
+const latestReleaseTag = await fetchLatestReleasedVersionNumber()
+const latestVersion = removeLeadingV(latestReleaseTag)
 const nextVersion = await determineNextVersionNumber(latestVersion)
 
-await pullLastVersion()
-    .then(bumpVersionInFiles)
-    .then(commitAndPush)
-    .then(release)
+await assertChangesSinceRelease(latestReleaseTag)
+await bumpVersionInFiles()
+await commitAndPush()
+await release()
 
 function getVersionToBump() {
     if (!argv.v || !(argv.v === 'minor' || argv.v === 'major')) {
@@ -44,8 +28,7 @@ function removeLeadingV(tagName) {
 
 async function fetchLatestReleasedVersionNumber() {
     let result = await $`gh release view --json tagName`
-    let tagName = JSON.parse(result).tagName
-    return removeLeadingV(tagName)
+    return JSON.parse(result).tagName
 }
 
 async function determineNextVersionNumber(previous) {
@@ -69,11 +52,6 @@ async function replaceInFile(filename, latestVersion, nextVersion) {
         .then(() => gitAdd(filename))
 }
 
-async function pullLastVersion() {
-    await $`git checkout master`
-    await $`git pull origin master`
-}
-
 async function gitAdd(filename) {
     return await $`git add ${filename}`
 }
@@ -83,8 +61,24 @@ async function commitAndPush() {
     await $`git push origin master`
 }
 
+async function getCurrentCommitHash() {
+  return (await $`git rev-parse HEAD`).stdout.trim()
+}
+
+async function getCommitHashOfTag(tag) {
+  return (await $`git rev-list -n 1 ${tag}`).stdout.trim()
+}
+
+async function assertChangesSinceRelease(releaseTag) {
+  let masterCommitHash = await getCurrentCommitHash()
+  let releaseCommitHash = await getCommitHashOfTag(releaseTag)
+  if(masterCommitHash === releaseCommitHash) {
+    throw new Error("No changes on master since release tagged " + releaseTag)
+  }
+}
+
 async function release() {
-    const newTag = 'v' + nextVersion
+  const newTag = 'v' + nextVersion
     return await $`gh release create ${newTag} --generate-notes`.catch(error => {
         console.error('something went wrong while creating the release. Please revert the version change!')
         throw error

9ec806790d7c7ad0a98feab7030125bab7bcd09b
mroloux <matti@seats.io>
2024-01-17 15:15:03 +0100

    Added methods to list event log items

---
+++ SeatsioDotNet.Test/EventLog/ListEventLogItemsTest.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using Xunit;
+
+namespace SeatsioDotNet.Test.EventLog;
+
+public class ListEventLogItemsTest : SeatsioClientTest
+{
+    [Fact]
+    public void Test()
+    {
+        var chart = Client.Charts.Create();
+        Client.Charts.Update(chart.Key, "a chart");
+
+        Thread.Sleep(2000);
+
+        var eventLogItems = Client.EventLog.ListAll();
+
+        Assert.Equal(new[] {"chart.created", "chart.published"}, eventLogItems.Select(e => e.Type));
+    }
+
+    [Fact]
+    public void Properties()
+    {
+        var chart = Client.Charts.Create();
+
+        Thread.Sleep(2000);
+
+        var eventLogItem = Client.EventLog.ListAll().ToList()[0];
+
+        Assert.True(eventLogItem.Id > 0);
+        Assert.Equal("chart.created", eventLogItem.Type);
+        Assert.Equal(Workspace.Key, eventLogItem.WorkspaceKey);
+        Assert.Equal(new Dictionary<string, object> {{"key", chart.Key}}, eventLogItem.Data);
+    }
+}
---
+++ SeatsioDotNet/EventLog/EventLog.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using RestSharp;
+using SeatsioDotNet.Util;
+
+namespace SeatsioDotNet.EventLog;
+
+public class EventLog
+{
+    private readonly RestClient _restClient;
+
+    public EventLog(RestClient restClient)
+    {
+        _restClient = restClient;
+    }
+
+    public IEnumerable<EventLogItem> ListAll()
+    {
+        return ParametrizedList().All();
+    }
+
+    public Page<EventLogItem> ListFirstPage(int? pageSize = null)
+    {
+        return ParametrizedList().FirstPage(pageSize);
+    }
+
+    public Page<EventLogItem> ListPageAfter(long id, int? pageSize = null)
+    {
+        return ParametrizedList().PageAfter(id, pageSize);
+    }
+
+    public Page<EventLogItem> ListPageBefore(long id, int? pageSize = null)
+    {
+        return ParametrizedList().PageBefore(id, pageSize);
+    }
+
+    private EventLogItemLister ParametrizedList()
+    {
+        return new EventLogItemLister(new PageFetcher<EventLogItem>(_restClient, "/event-log"));
+    }
+}

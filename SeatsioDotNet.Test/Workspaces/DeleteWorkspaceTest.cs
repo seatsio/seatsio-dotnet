7c0a32b498441adb749615bc6a740eed0bd24d92
Steve Chaloner <steve@seats.io>
2025-06-16 16:47:50 +0200

    Delete workspaces
    This PR exposes workspace deletion for inactive workspaces.

---
+++ SeatsioDotNet.Test/Workspaces/DeleteWorkspaceTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SeatsioDotNet.Test.Workspaces;
+
+public class DeleteWorkspaceTest : SeatsioClientTest
+{
+    [Fact]
+    public async Task TestDeleteInactiveWorkspace()
+    {
+        var workspace = await Client.Workspaces.CreateAsync("a ws");
+        await Client.Workspaces.DeactivateAsync(workspace.Key);
+
+        await Client.Workspaces.DeleteAsync(workspace.Key);
+
+        Exception ex = await Assert.ThrowsAsync<SeatsioException>(async () => await Client.Workspaces.RetrieveAsync(workspace.Key));
+        Assert.Equal("No workspace found with public key '" + workspace.Key + "'.", ex.Message);
+    }
+
+    [Fact]
+    public async Task TestDeleteActiveWorkspace()
+    {
+        var workspace = await Client.Workspaces.CreateAsync("a ws");
+        
+        Exception ex = await Assert.ThrowsAsync<SeatsioException>(async () => await Client.Workspaces.DeleteAsync(workspace.Key));
+        Assert.Equal("Cannot delete active workspace, please deactivate it first.", ex.Message);
+    }
+}

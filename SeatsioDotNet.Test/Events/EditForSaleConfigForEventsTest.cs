012b0e1f1cd015aa16718355717068590451479f
mroloux <matti@seats.io>
2025-11-10 17:33:05 +0100

    Refactoring

--- SeatsioDotNet.Test/Events/EditForSaleConfigForEventsTest.cs
+++ SeatsioDotNet.Test/Events/EditForSaleConfigForEventsTest.cs
@@ -15,13 +15,12 @@ public class EditForSaleConfigForEventsTest : SeatsioClientTest
         ForSaleConfig forSaleConfig = new ForSaleConfig().WithForSale(false).WithObjects(new[] { "A-1", "A-2", "A-3" });
         var evnt1 = await Client.Events.CreateAsync(chartKey, new CreateEventParams().WithForSaleConfig(forSaleConfig));
         var evnt2 = await Client.Events.CreateAsync(chartKey, new CreateEventParams().WithForSaleConfig(forSaleConfig));
-        var request = new Dictionary<string, ForSaleAndNotForSale>
+
+        await Client.Events.EditForSaleConfigForEventsAsync(new Dictionary<string, ForSaleAndNotForSale>
         {
             { evnt1.Key, new ForSaleAndNotForSale(new[] { new ObjectAndQuantity("A-1") }, null) },
             { evnt2.Key, new ForSaleAndNotForSale(new[] { new ObjectAndQuantity("A-2") }, null) }
-        };
-
-        await Client.Events.EditForSaleConfigForEventsAsync(request);
+        });
 
         var retrievedForSaleConfig1 = (await Client.Events.RetrieveAsync(evnt1.Key)).ForSaleConfig;
         Assert.False(retrievedForSaleConfig1.ForSale);
@@ -39,13 +38,12 @@ public class EditForSaleConfigForEventsTest : SeatsioClientTest
         ForSaleConfig forSaleConfig = new ForSaleConfig().WithForSale(false).WithObjects(new[] { "A-1", "A-2", "A-3" });
         var evnt1 = await Client.Events.CreateAsync(chartKey, new CreateEventParams().WithForSaleConfig(forSaleConfig));
         var evnt2 = await Client.Events.CreateAsync(chartKey, new CreateEventParams().WithForSaleConfig(forSaleConfig));
-        var request = new Dictionary<string, ForSaleAndNotForSale>
+
+        var result = await Client.Events.EditForSaleConfigForEventsAsync(new Dictionary<string, ForSaleAndNotForSale>
         {
             { evnt1.Key, new ForSaleAndNotForSale(new[] { new ObjectAndQuantity("A-1") }, null) },
             { evnt2.Key, new ForSaleAndNotForSale(new[] { new ObjectAndQuantity("A-2") }, null) }
-        };
-
-        var result = await Client.Events.EditForSaleConfigForEventsAsync(request);
+        });
 
         Assert.False(result[evnt1.Key].ForSaleConfig.ForSale);
         Assert.Equal(new[] { "A-2", "A-3" }, result[evnt1.Key].ForSaleConfig.Objects);
@@ -62,13 +60,12 @@ public class EditForSaleConfigForEventsTest : SeatsioClientTest
         var chartKey = CreateTestChart();
         var evnt1 = await Client.Events.CreateAsync(chartKey);
         var evnt2 = await Client.Events.CreateAsync(chartKey);
-        var request = new Dictionary<string, ForSaleAndNotForSale>
+
+        await Client.Events.EditForSaleConfigForEventsAsync(new Dictionary<string, ForSaleAndNotForSale>
         {
             { evnt1.Key, new ForSaleAndNotForSale(null, new[] { new ObjectAndQuantity("A-1") }) },
             { evnt2.Key, new ForSaleAndNotForSale(null, new[] { new ObjectAndQuantity("A-2") }) }
-        };
-
-        await Client.Events.EditForSaleConfigForEventsAsync(request);
+        });
 
         var retrievedForSaleConfig1 = (await Client.Events.RetrieveAsync(evnt1.Key)).ForSaleConfig;
         Assert.False(retrievedForSaleConfig1.ForSale);
